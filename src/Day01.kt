39be59e120d917de29511de4ef57cbcd80ed33d8
Julian Toledo <jtoledo@google.com>
2022-12-01 15:09:15 +0100

    Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,32 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    fun elfCalloriesList(input: List<String>): MutableList<Int> {
+        var elfCalories: MutableList<Int> = mutableListOf()
+        var indexElf = 0
+        var sumCalories = 0
+        for (i in input) {
+            var calories = i.toIntOrNull()
+            if (calories != null && calories > 0) {
+                sumCalories += calories
+            } else {
+                elfCalories.add(sumCalories)
+                sumCalories = 0
+            }
+        }
+        return elfCalories
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part1(elfCalories: List<Int>): Int {
+        return elfCalories.max()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun part2(elfCalories: MutableList<Int>): Int {
+        elfCalories.sortDescending()
+        return elfCalories[0] + elfCalories[1] + elfCalories[2]
+    }
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val input = readInput("Day01_test")
+    var elfCalories = elfCalloriesList(input)
+    println( part1(elfCalories))
+    println( part2(elfCalories))
 }
